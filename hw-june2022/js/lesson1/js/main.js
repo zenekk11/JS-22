e10b005731e0f5f28346a4a3257127f36c5e450c
zenekk11 <it.kenez@gmail.com>
2022-07-22 18:17:36 +0300

    home works

---
+++ hw-june2022/js/lesson1/js/main.js
@@ -0,0 +1,26 @@
+let foo = 'hello'
+console.log(foo);
+foo = 'owu'
+console.log(foo);
+foo = 'com'
+console.log(foo);
+foo = 'ua'
+console.log(foo);
+
+let a = 1
+console.log(a);
+b = 10
+console.log(b);
+c = -999
+console.log(c);
+d = 123
+console.log(d);
+e = 3.14
+console.log(e);
+f = 2.7
+console.log(f);
+g = 16
+console.log(g);
+
+
+true, false;
