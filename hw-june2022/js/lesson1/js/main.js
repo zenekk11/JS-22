a5aa53af89e5dff3fede12af00401546d23e90ac
zenekk11 <it.kenez@gmail.com>
2022-07-22 23:33:56 +0300

    home works

--- hw-june2022/js/lesson1/js/main.js
+++ hw-june2022/js/lesson1/js/main.js
@@ -1,26 +1,44 @@
-let foo = 'hello'
-console.log(foo);
-foo = 'owu'
-console.log(foo);
-foo = 'com'
-console.log(foo);
-foo = 'ua'
-console.log(foo);
+let str1 = 'hello';
+let str2 = 'owu';
+let str3 = 'com';
+let str4 = 'ua';
+let nb1 = 1;
+let nb2 = 10;
+let nb3 = -999;
+let nb4 = 123;
+let nb5 = 3.14
+let nb6 = 2.7
+let nb7 = 16
+let bl1 = 'true';
+let bl2 = 'false';
 
-let a = 1
-console.log(a);
-b = 10
-console.log(b);
-c = -999
-console.log(c);
-d = 123
-console.log(d);
-e = 3.14
-console.log(e);
-f = 2.7
-console.log(f);
-g = 16
-console.log(g);
+let space = ' ';
+let sent = str1 + space + str2 + space + str3 + space + str4;
+console.log(sent);
+alert(sent);
+document.write(sent);
+
+let numb = nb1 + space + nb2 + space + nb3 + space + nb4 + space + nb5 + space + nb6 + space + nb7;
+console.log(numb);
+alert(numb);
+document.write(numb);
+
+let bool = bl1 + space + bl2;
+console.log(bool);
+alert(bool);
+document.write(bool);
 
 
-true, false;
+let firstName = 'Zenovii';
+middleName = 'Bogdanovych';
+lastName = 'Kondzelko';
+space = ' ';
+person = firstName + space + middleName + space + lastName;
+console.log(person);
+document.write(person)
+
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a, b, c);
