62de0472e22652b62e5087d2ef8f0b3fef92b35e
zenekk11 <it.kenez@gmail.com>
2022-08-03 18:44:48 +0300

    home works

--- hw-june2022/js/lesson4/js/main.js
+++ hw-june2022/js/lesson4/js/main.js
@@ -115,9 +115,9 @@
 // ];
 //
 // for (const product of products) {
-//     document.write(`<div>
-//     <h3>${product.title} - ${product.price}</h3>
-//     <img src="${product.image}" alt="">
+//     document.write(`<div class="product-card">
+//     <h3 class="product-title">${product.title}. Price - ${product.price}</h3>
+//     <img src="${product.image}" alt="" class="product-image">
 // </div>`);
 // }
 
@@ -142,18 +142,37 @@
 // - користувачів зі статусом false
 // - користувачів які старші за 30 років
 
-let users = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
-];
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+//
+// for (const user of users) {
+//     if (user.status) {
+//         console.log(user);
+//     }
+// }
+//
+// for (const user of users) {
+//     if (!user.status) {
+//         console.log(user);
+//     }
+// }
+//
+// for (const user of users) {
+//     if (user.age > 30) {
+//         console.log(user);
+//     }
+// }
+
 
 
